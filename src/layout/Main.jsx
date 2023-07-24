b072ac5f5e54910a16b003da9c93726fdc71a680
JannatunNimeNishat <nishatnime100@gmail.com>
2023-07-24 15:45:41 +0600

    project starter

---
+++ src/layout/Main.jsx
@@ -0,0 +1,15 @@
+import Footer from "../pages/Share/Footer/Footer";
+import Navbar from "../pages/Share/Navbar/Navbar";
+
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2>THis is main router</h2>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
