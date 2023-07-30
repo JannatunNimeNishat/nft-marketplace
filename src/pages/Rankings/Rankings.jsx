95e310742c4850db5d6e2efa4e5b65a2831e0e26
JannatunNimeNishat <nishatnime100@gmail.com>
2023-07-30 20:11:06 +0600

    rankings page added

--- src/pages/Rankings/Rankings.jsx
+++ src/pages/Rankings/Rankings.jsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 
 
 const Rankings = () => {
-    const [activeTab, setActiveTab] = useState(1)
+    const [activeTab, setActiveTab] = useState(1);
+    const [artists, setArtists] = useState([])
+    useEffect(() => {
+        fetch('../../../public/json/artists.json')
+            .then(res => res.json())
+            .then(data => setArtists(data))
+    }, [])
     return (
         <div className="lg:w-[1000px]  lg:py-[50px] mx-auto px-5 lg:px-0 ">
             <h3 className="text-[22px] lg:text-[51px] font-bold">Top Creators</h3>
             <p className="text-[16px] lg:text-[22px] pt-1">Check out top ranking NFT artists on the NFT Marketplace.</p>
-            {/*  */}
+            {/* tab headers */}
             <div className=' pt-10 lg:pt-20  flex  gap-8 lg:gap-3 w-11/12 lg:w-full  mx-auto'>
 
                 {/* Today */}
@@ -80,10 +88,292 @@ const Rankings = () => {
                         }
                     `} ></div>
                 </div>
+            </div>
+            {/* tab contents */}
+            <div className="mt-10">
+
+                {/* today table  */}
+                <div className={`overflow-x-auto 
+                ${activeTab === 1 ?
+                        'block'
+                        :
+                        'hidden'
+                    }
+                `}>
+                    <table className="table text-white ">
+                        {/* head */}
+                        <thead className="text-[#858584]">
+
+                            <tr className="border border-[#3B3B3B] ">
+                                <th className="">
+                                    #
+                                </th>
+                                <th>Artist</th>
+                                <th>Change</th>
+                                <th>NFTs Sold</th>
+                                <th>Volume</th>
+                            </tr>
+
+                        </thead>
+                        <tbody>
+
+
+                            {/* row 1 */}
+                            {artists.map((singleArtist, index) => <>
+                                <br key={index}/>
+                                <tr className="bg-[#3B3B3B] ">
+                                    <th className="">
+                                        <p className="bg-[#2B2B2B] text-[#858584] rounded-full w-[30px] h-[30px] flex items-center justify-center">{index + 1}</p>
+                                    </th>
+                                    <td className="cursor-pointer">
+                                        <Link to={`/artist_page/${singleArtist._id}`}>
+                                            <div className="flex items-center space-x-3 ">
+                                                <div className="avatar ">
+                                                    <div className="mask mask-squircle w-12 h-12 ">
+                                                        <img className="" src={singleArtist.artist_img} alt="Avatar " />
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <div className="font-bold hover:text-red-500">{singleArtist.artist_name}</div>
+
+                                                </div>
+                                            </div>
+                                        </Link>
+                                    </td>
+
+                                    <td>
+                                        <p className="text-[#00AC4F]">+1.41%</p>
+
+                                    </td>
+
+                                    <td className="">602</td>
+                                    <td className=""><span>12.4</span> <span className="pl-1">ETH</span></td>
+
+
+
+                                </tr>
+                            </>)}
+
+                        </tbody>
+
+
+                    </table>
+                </div>
+                {/* this week table  */}
+                <div className={`overflow-x-auto 
+                ${activeTab === 2 ?
+                        'block'
+                        :
+                        'hidden'
+                    }
+                `}>
+                    <table className="table text-white ">
+                        {/* head */}
+                        <thead className="text-[#858584]">
+
+                            <tr className="border border-[#3B3B3B] ">
+                                <th className="">
+                                    #
+                                </th>
+                                <th>Artist</th>
+                                <th>Change</th>
+                                <th>NFTs Sold</th>
+                                <th>Volume</th>
+                            </tr>
+
+                        </thead>
+                        <tbody>
+
+                            <br />
+                            {/* row 1 */}
+                            {artists.slice(2,5).map((singleArtist, index) => <>
+                                <br key={index} />
+                                <tr  className="bg-[#3B3B3B] ">
+                                    <th className="">
+                                        <p className="bg-[#2B2B2B] text-[#858584] rounded-full w-[30px] h-[30px] flex items-center justify-center">{index + 1}</p>
+                                    </th>
+                                    <td className="cursor-pointer">
+                                        <Link to={`/artist_page/${singleArtist._id}`}>
+                                            <div className="flex items-center space-x-3 ">
+                                                <div className="avatar ">
+                                                    <div className="mask mask-squircle w-12 h-12 ">
+                                                        <img className="" src={singleArtist.artist_img} alt="Avatar " />
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <div className="font-bold hover:text-red-500">{singleArtist.artist_name}</div>
+
+                                                </div>
+                                            </div>
+                                        </Link>
+                                    </td>
+
+                                    <td>
+                                        <p className="text-[#00AC4F]">+1.41%</p>
+
+                                    </td>
+
+                                    <td className="">602</td>
+                                    <td className=""><span>12.4</span> <span className="pl-1">ETH</span></td>
+
+
+
+                                </tr>
+                            </>)}
+
+
+                        </tbody>
+
+
+                    </table>
+                </div>
+                {/* this months table  */}
+                <div className={`overflow-x-auto 
+                ${activeTab === 3 ?
+                        'block'
+                        :
+                        'hidden'
+                    }
+                `}>
+                    <table className="table text-white ">
+                        {/* head */}
+                        <thead className="text-[#858584]">
+
+                            <tr className="border border-[#3B3B3B] ">
+                                <th className="">
+                                    #
+                                </th>
+                                <th>Artist</th>
+                                <th>Change</th>
+                                <th>NFTs Sold</th>
+                                <th>Volume</th>
+                            </tr>
+
+                        </thead>
+                        <tbody>
+
+                           
+                            {/* row 1 */}
+                            {artists.slice(3,5).map((singleArtist, index) => <>
+                                <br key={index}/>
+                                <tr  className="bg-[#3B3B3B] ">
+                                    <th className="">
+                                        <p className="bg-[#2B2B2B] text-[#858584] rounded-full w-[30px] h-[30px] flex items-center justify-center">{index + 1}</p>
+                                    </th>
+                                    <td className="cursor-pointer">
+                                        <Link to={`/artist_page/${singleArtist._id}`}>
+                                            <div className="flex items-center space-x-3 ">
+                                                <div className="avatar ">
+                                                    <div className="mask mask-squircle w-12 h-12 ">
+                                                        <img className="" src={singleArtist.artist_img} alt="Avatar " />
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <div className="font-bold hover:text-red-500">{singleArtist.artist_name}</div>
+
+                                                </div>
+                                            </div>
+                                        </Link>
+                                    </td>
+
+                                    <td>
+                                        <p className="text-[#00AC4F]">+1.41%</p>
+
+                                    </td>
+
+                                    <td className="">602</td>
+                                    <td className=""><span>12.4</span> <span className="pl-1">ETH</span></td>
+
+
+
+                                </tr>
+                            </>)}
+                           
+
+
+                        </tbody>
+
+
+                    </table>
+                </div>
+                {/* all time table  */}
+                <div className={`overflow-x-auto 
+                ${activeTab === 4 ?
+                        'block'
+                        :
+                        'hidden'
+                    }
+                `}>
+                    <table className="table text-white ">
+                        {/* head */}
+                        <thead className="text-[#858584]">
+
+                            <tr className="border border-[#3B3B3B] ">
+                                <th className="">
+                                    #
+                                </th>
+                                <th>Artist</th>
+                                <th>Change</th>
+                                <th>NFTs Sold</th>
+                                <th>Volume</th>
+                            </tr>
+
+                        </thead>
+                        <tbody>
+
+                           
+                            {/* row 1 */}
+                            {artists.slice(0,3).map((singleArtist, index) => <>
+                                <br key={index}/>
+                                <tr  className="bg-[#3B3B3B] ">
+                                    <th className="">
+                                        <p className="bg-[#2B2B2B] text-[#858584] rounded-full w-[30px] h-[30px] flex items-center justify-center">{index + 1}</p>
+                                    </th>
+                                    <td className="cursor-pointer">
+                                        <Link to={`/artist_page/${singleArtist._id}`}>
+                                            <div className="flex items-center space-x-3 ">
+                                                <div className="avatar ">
+                                                    <div className="mask mask-squircle w-12 h-12 ">
+                                                        <img className="" src={singleArtist.artist_img} alt="Avatar " />
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <div className="font-bold hover:text-red-500">{singleArtist.artist_name}</div>
+
+                                                </div>
+                                            </div>
+                                        </Link>
+                                    </td>
+
+                                    <td>
+                                        <p className="text-[#00AC4F]">+1.41%</p>
+
+                                    </td>
+
+                                    <td className="">602</td>
+                                    <td className=""><span>12.4</span> <span className="pl-1">ETH</span></td>
+
+
+
+                                </tr>
+                            </>)}
+                          
+                           
+
+
+                        </tbody>
+
+
+                    </table>
+                </div>
+
+
 
 
 
             </div>
+
+
         </div>
     );
 };
